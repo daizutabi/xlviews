4044a9cc6362ea04f011c98012e8f5ac4070949d
Daizu <daizutabi@gmail.com>
2025-02-09 14:50:28 +0900

    Add list-based aggregation tests for SheetFrame
    - Implement test for aggregating with multiple functions using a list

--- tests/dataframes/agg/test_wide_column.py
+++ tests/dataframes/agg/test_wide_column.py
@@ -29,19 +29,22 @@ def df(sf: SheetFrame):
     return sf.data
 
 
-def test_sf_data(df: DataFrame):
-    print(df)
-    assert 0
-
-
 @pytest.mark.parametrize("func", ["sum", "max", "mean"])
 def test_str(sf: SheetFrame, df: DataFrame, func: str):
     a = sf.agg(func, formula=True)
     b = df.agg(func)
-    print(a)
-    print(b)
-    assert 0
     assert isinstance(a, Series)
     assert a.index.to_list() == b.index.to_list()
     sf = SheetFrame(20, 2, data=a, sheet=sf.sheet, style=False)
     np.testing.assert_array_equal(sf.data[0], b)
+
+
+def test_list(sf: SheetFrame, df: DataFrame):
+    func = ["min", "max", "median", "sum"]
+    a = sf.agg(func, formula=True)
+    b = df.agg(func)  # type: ignore
+    assert isinstance(a, DataFrame)
+    assert a.index.to_list() == b.index.to_list()
+    assert a.columns.to_list() == b.columns.to_list()
+    sf = SheetFrame(50, 2, data=a, sheet=sf.sheet, style=False)
+    np.testing.assert_array_equal(sf.data, b)
