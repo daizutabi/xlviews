0840a99cba5fc2c3fe2e55856a65dc3eb71afbdd
Daizu <daizutabi@gmail.com>
2025-02-15 18:35:29 +0900

    Remove range-related methods from GroupBy class
    - Delete `range()`, `first_range()`, `ranges()`, and `first_ranges()` methods
    - Simplify `__iter__()` method to return keys directly
    - Minor variable name refactoring in aggregation method

--- tests/dataframes/groupby/test_ranges.py
+++ tests/dataframes/groupby/test_ranges.py
@@ -3,7 +3,6 @@ import pytest
 from pandas import DataFrame
 from xlwings import Sheet
 
-from xlviews.core.range_collection import RangeCollection
 from xlviews.dataframes.groupby import GroupBy
 from xlviews.dataframes.sheet_frame import SheetFrame
 from xlviews.testing import is_excel_installed
@@ -51,29 +50,3 @@ def gr(sf: SheetFrame):
 def test_group_key(gr: GroupBy):
     keys = list(gr.group.keys())
     assert keys == [("a", "c"), ("a", "d"), ("b", "c"), ("b", "d")]
-
-
-def test_ranges_len(gr: GroupBy):
-    assert len(list(gr.ranges("a"))) == 4
-
-
-@pytest.mark.parametrize(("column", "c"), [("x", "C"), ("y", "D")])
-def test_first_ranges(gr: GroupBy, column, c):
-    rs = [r.get_address() for r in gr.first_ranges(column)]
-    assert rs == [f"${c}$4", f"${c}$8", f"${c}$12", f"${c}$16"]
-
-
-@pytest.mark.parametrize(("column", "c"), [("a", "F"), ("b", "G"), ("c", "H")])
-@pytest.mark.parametrize(
-    ("k", "a"),
-    [
-        (0, "${c}$4:${c}$7,${c}$20:${c}$23"),
-        (1, "${c}$8:${c}$11"),
-        (2, "${c}$12:${c}$15"),
-        (3, "${c}$16:${c}$19"),
-    ],
-)
-def test_ranges(gr: GroupBy, column, c, k: int, a):
-    rc = list(gr.ranges(column))[k]
-    assert isinstance(rc, RangeCollection)
-    assert rc.get_address() == a.format(c=c)
